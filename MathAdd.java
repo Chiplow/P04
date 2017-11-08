5bd34e334bfe2cb2114c807086e4e0d153c8ec60
165288P LOW JUN JIE <165288P@sit.nyp.edu.sg>
2017-11-08 11:18:31 +0800

    test

--- MathAdd.java
+++ MathAdd.java
@@ -1,8 +1,11 @@
 public class MathAdd {
 	
+	
 	public MathAdd {}
 	
-	static public int add(int a, int b) {   
+	static public int add(int a, int b) { 
+		System.out.println(a + b);
         return a + b; 
+		
 	}
 }
